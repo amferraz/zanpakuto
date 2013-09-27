496ddea24ca8b516c2ee41755a503f9d6ae3558f
Anderson Ferraz <caco@jusbrasil.com.br>
2013-09-27 15:45:25 -0300

    Remove long description
    It seems that it doesn't play well with Travis

--- setup.py
+++ setup.py
@@ -2,17 +2,12 @@
 
 from setuptools import setup
 
-long_description = (open('README.md').read() + '\n\n' +
-                    open('CHANGES.md').read()
-                    )
-
 setup(
     name="zanpakuto",
     version='0.2',
     author="Anderson Ferraz",
     author_email="amarquesferraz@gmail.com",
     description="A small utility to simplify HTML excerpts",
-    long_description=long_description,
     url="https://github.com/amferraz/zanpakuto",
     download_url='https://github.com/amferraz/zanpakuto/archive/0.2.zip',
     packages=['zanpakuto'],
