63287f938ffcc9ee2f94fe24b9eee7f53a57ce4e
Anderson Ferraz <caco@jusbrasil.com.br>
2013-09-27 10:40:33 -0300

    Fix package download_url

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email="amarquesferraz@gmail.com",
     description="A small utility to simplify HTML excerpts",
     url="https://github.com/amferraz/zanpakuto",
+    download_url='https://github.com/amferraz/zanpakuto/archive/0.1.zip',
     packages=['zanpakuto'],
-    install_requires=['lxml==3.2.3']
+    install_requires=['lxml==3.2.3'],
 )
